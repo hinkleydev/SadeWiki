861a97bf5a9b4dc8a405ddaadf5fa07a9e4f6a75
hinkleydev <azz@hinkley.dev>
2025-10-30 19:58:31 +0000

    [#58] FEAT: Return directories

--- functions.py
+++ functions.py
@@ -4,7 +4,10 @@ import os
 def recursively_get_files(path):
     walk_list = os.walk(path)
     file_list = []
+    dir_list = []
     for (root, dirs, files) in walk_list:
         for item in files:
             file_list.append(os.path.join(root, item))
-    return file_list
+            dir_list.append(root)
+    dir_list = list(set(dir_list)) # no duplicates
+    return file_list, dir_list
